d6638f32cfe57d156f317dbd7ac266a8bbbc4ba9
Noah Ruppe <noah.ruppe30@gmail.com>
2026-04-20 13:33:04 -0400

    changes

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-const databaseUrl = process.env["DATABASE_URL"] || "file:./dev.db";
+const databaseUrl = process.env["POSTGRES_PRISMA_URL"] || process.env["DATABASE_URL"] || "postgresql://localhost/flashcard";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
